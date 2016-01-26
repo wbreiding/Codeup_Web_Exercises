7b1fd50413b36302696d77e63f8d2492a8dd0393
wbreiding <wdibean@gmail.com>
2016-01-26 12:45:22 -0600

    POST Requests

---
+++ public/authorized.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>Authorized</title>
+</head>
+
+<body>
+
+<h3>Authorized</h3>
+
+
+</body>
+
+</html>
---
+++ public/login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>Login</title>
+  <style>
+    #message {
+      color: red;
+      font-size: 16px;
+    }
+  </style>
+</head>
+
+<body>
+
+<h3>User Login</h3>
+
+<?php
+
+$username = isset($_POST['username']) ? $_POST['username'] : '';
+$password = isset($_POST['password']) ? $_POST['password'] : '';
+$errorMessage = "";
+
+if ($username == 'guest' && $password == 'password') {
+  header("Location: authorized.php");
+} elseif ($username == '' && $password == '') {
+  $errorMessage = "";
+} else {
+  $errorMessage = "Login Failed. Please try again.";
+}
+
+ ?>
+
+<div id="message"><?= $errorMessage ?></div>
+
+<form method="post" action="login.php">
+  <label>Username</label>
+  <input type="text" name="username" /><br />
+  <label>Password</label>
+  <input type="text" name="password" /><br />
+  <input type="submit" name="submit" />
+</form>
+
+</body>
+
+</html>
