5b4fc8721bdc2db2444b51885a8c4338213e8489
wbreiding <wdibean@gmail.com>
2016-01-28 11:11:23 -0600

    include/require, part2

---
+++ public/footer.php
@@ -0,0 +1,5 @@
+
+<hr style="color:black; width:90%;" />
+<div style="font-size:8px;color:#ddd">Copyright information goes here.  For Problems, go away! I'm not giving you my email address.</div>
+</body>
+</html>
---
+++ public/navbar.php
@@ -0,0 +1,6 @@
+
+<div id="navbar">
+<a href="index.php">Home</a><br />
+<a href="about.php">About us</a><br />
+<a href="otherStuff.php">Other Stuff</a><br />
+</div>
