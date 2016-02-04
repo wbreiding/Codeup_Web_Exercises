3f3b00741f1f60ccec716167a94ab1a5c73d0460
wbreiding <wdibean@gmail.com>
2016-02-04 09:48:41 -0600

    prepared statements, done

--- public/national_parks.php
+++ public/national_parks.php
@@ -10,23 +10,53 @@
       border-color: black;
       border-collapse: collapse;
     }
-    #odd {background-color: #ccc};
+    #odd {background-color: #ccc;}
+    #error {color: red;}
   </style>
 </head>
 
 <body>
+  <?php
+  require "../Input.php";
+
+  define('DB_HOST', '127.0.0.1');
+  define('DB_NAME', 'parks_db');
+  define('DB_USER', 'parks_user');
+  define('DB_PASS', 'parks');
+  require "../db_connect.php";
+
+  //put handling of insert here
+  $errorMessage = "";
+  if (isset($_REQUEST['submit'])) {
+      $errorMessage = $errorMessage . (Input::has('name') ? "" : "Name is not set<br />");
+      $errorMessage = $errorMessage . (Input::has('location') ? "" : "Location is not set<br />");
+      $errorMessage = $errorMessage . (Input::has('date_established') ? "" : "Date Established is not set<br />");
+      $errorMessage = $errorMessage . (Input::has('area_in_acres') ? "" : "Area in Acres is not set<br />");
+      $errorMessage = $errorMessage . (Input::has('description') ? "" : "Description is not set<br />");
+      $inputDate = htmlspecialchars(strip_tags(Input::get('date_established')));
+      $inputDate = date('Y-m-d', strtotime($inputDate)); 
+      $errorMessage = $errorMessage . ((bool)strtotime($inputDate) ? "" : "{$inputDate} is not a valid date.<br />");
+      $errorMessage = $errorMessage . (is_numeric(Input::get('area_in_acres')) ? "" : "Area must be a valid number.<br />");
+      if($errorMessage == "") {
+        $dataArray = array(htmlspecialchars(strip_tags(Input::get('name'))),htmlspecialchars(strip_tags(Input::get('location'))), $inputDate, htmlspecialchars(strip_tags(Input::get('area_in_acres'))), htmlspecialchars(strip_tags(Input::get('description'))));
+        $insert = 'INSERT INTO national_parks (name,location,date_established, area_in_acres, description) VALUES (?,?,?,?,?)';
+        $stmt = $dbc->prepare($insert);
+        $stmt->execute($dataArray);
+
+        if ($stmt->rowCount() != 1) {
+          $errorMessage = "There was an error inserting the new row.";
+        } else {
+          $errorMessage = "Your entry has been submitted successfully.";
+        }
+      }
+  }
+   ?>
+  <div id="error"><?=$errorMessage ?></div>
   <table>
     <tr>
       <th>Park</th> <th>Location</th> <th>Date Established</th> <th>Area in Acres</th> <th>Description</th>
     </tr>
 <?php
-require "../Input.php";
-
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'parks_db');
-define('DB_USER', 'parks_user');
-define('DB_PASS', 'parks');
-require "../db_connect.php";
 
 if (isset($_REQUEST['page'])) {
   $page = Input::get('page');
@@ -38,9 +68,10 @@ $offset = ($page-1)*4;
 $previous = $page-1;
 $next = $page+1;
 
-$query = "SELECT id, name, location, date_established, area_in_acres, description FROM national_parks LIMIT 4 OFFSET ?";
+$query = 'SELECT id, name, location, date_established, area_in_acres, description FROM national_parks LIMIT 4 OFFSET :offset';
 $stmt = $dbc->prepare($query);
-$stmt->execute(array($offset))->fetchAll(PDO::FETCH_ASSOC);
+$stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
+$stmt->execute();
 $count = $dbc->query("SELECT * FROM national_parks")->rowCount();
 
 $maxPage = $count/4;
@@ -50,8 +81,20 @@ $maxPage = $count/4;
     <td><?=$info['name']?></td> <td><?=$info['location'] ?></td>  <td><?=$info['date_established']?></td> <td><?=$info['area_in_acres']?></td> <td><?=$info['description']?></td>
   </tr>
 <?php endforeach ?>
+</table>
 
 <?php if ($page !=1): ?><a href='?page=<?= $previous ?>'>Previous Page</a> | <?php endif ?>
   <?php if ($page < $maxPage): ?> <a href='?page=<?= $next ?>'>Next Page</a> <?php endif ?>
+
+<p>
+  <form method="post" action="national_parks.php">
+  <label for="name">Name</label> <input type="text" name="name" /><br />
+  <label for="location">Location</label> <input type="text" name="location" /><br />
+  <label for="date_established">Date Established</label> <input type="text" name="date_established" /><br />
+  <label for="area_in_acres">Area In Acres</label> <input type="text" name="area_in_acres" /><br />
+  <label for="description">Description</label><textarea name="description"></textarea><br />
+  <input type="submit" name="submit" value="submit" />
+</form>
+</p>
 </body>
 </html>
